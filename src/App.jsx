0fc9353b668c3c5b0d68a9369b842cecd235a423
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-14 15:51:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  )
+}
+
+export default App
